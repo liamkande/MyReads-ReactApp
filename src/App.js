83d0653185a32ada3b8af32724b49263785adcde
Liam Kande <liamkande@Liams-MBP.fios-router.home>
2018-04-19 01:28:49 -0400

    created MovingBooks Handler func.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 
 class BooksApp extends Component {
-  state = {
-    currentlyReading: [],
-    wantToRead: [],
-    read:[]
-  }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' component={ListBooks}/>
-        <Route exact path='/search' component={SearchBooks}/>    
+        <Route exact path='/search' component={SearchBooks}/>
       </div>
     )
   }
